7ef55b02c664c2d50fd093d14e6a26ac9f50b1e6
Jinho D. Choi <jdchoi77@gmail.com>
2014-11-14 02:34:02 -0500

    Updated the structure package.

---
+++ src/main/java/edu/emory/clir/clearnlp/qa/structure/Instance.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright 2014, Emory University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.emory.clir.clearnlp.qa.structure;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import edu.emory.clir.clearnlp.qa.structure.attribute.AbstractAttribute;
+import edu.emory.clir.clearnlp.qa.structure.attribute.AttributeType;
+
+public class Instance implements Serializable
+{
+	private static final long serialVersionUID = 17479620890779053L;
+	private Map<AttributeType,AbstractAttribute> m_attributes;
+	private Map<SemanticType,List<Instance>>     m_predicates;
+	private Map<SemanticType,List<Instance>>     m_arguments;
+	
+	public Instance()
+	{
+		m_attributes = new HashMap<>();
+		m_predicates = new HashMap<>();
+		m_arguments  = new HashMap<>();
+	}
+	
+	public Set<AttributeType> getAttributeTypeSet()
+	{
+		return m_attributes.keySet();
+	}
+    
+	public AbstractAttribute getAttribute(AttributeType type)
+	{
+		return m_attributes.get(type);
+	}
+	
+	public AbstractAttribute putAttribute(AttributeType type, AbstractAttribute attribute)
+	{
+		return m_attributes.put(type, attribute);
+	}
+	
+	public AbstractAttribute removeAttribute(AttributeType type)
+	{
+		return m_attributes.remove(type);
+	}
+	
+	public Set<SemanticType> getPredicdateTypeSet()
+	{
+		return m_predicates.keySet();
+	}
+	
+	public List<Instance> getPredicateList(SemanticType type)
+	{
+		return m_predicates.get(type);
+	}
+	
+	public List<Instance> putPredicateList(SemanticType type, Instance predicate)
+	{
+		return putInstance(m_predicates, type, predicate);
+	}
+	
+	public boolean removePredicate(Instance predicate)
+	{
+		return removeInstance(m_predicates, predicate);
+	}
+	
+	public Set<SemanticType> getArgumentTypeSet()
+	{
+		return m_arguments.keySet();
+	}
+	
+	public List<Instance> getArgumentList(SemanticType type)
+	{
+		return m_arguments.get(type);
+	}
+	
+	public List<Instance> putArgumentList(SemanticType type, Instance argument)
+	{
+		return putInstance(m_arguments, type, argument);
+	}
+	
+	public boolean removeArgument(Instance argument)
+	{
+		return removeInstance(m_arguments, argument);
+	}
+	
+	private List<Instance> putInstance(Map<SemanticType,List<Instance>> map, SemanticType type, Instance instance)
+	{
+		List<Instance> arguments = map.get(type);
+		
+		if (arguments == null)
+		{
+			arguments = new ArrayList<>();
+			m_arguments.put(type, arguments);
+		}
+		
+		arguments.add(instance);
+		return arguments;
+	}
+	
+	public boolean removeInstance(Map<SemanticType,List<Instance>> map, Instance instance)
+	{
+		List<Instance> list;
+		
+		for (SemanticType type : map.keySet())
+		{
+			list = map.get(type);
+			
+			for (Instance i : list)
+			{
+				if (i == instance)
+				{
+					list.remove(instance);
+					if (list.isEmpty()) map.remove(type);
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+}
---
+++ src/main/java/edu/emory/clir/clearnlp/qa/structure/document/EnglishDocument.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2014, Emory University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.emory.clir.clearnlp.qa.structure.document;
+
+import edu.emory.clir.clearnlp.dependency.DEPNode;
+import edu.emory.clir.clearnlp.dependency.DEPTree;
+
+/**
+ * @author Jinho D. Choi ({@code jinho.choi@emory.edu})
+ */
+public class EnglishDocument extends AbstractDocument
+{
+	private static final long serialVersionUID = -1190545348244741736L;
+
+	@Override
+	public void addInstances(DEPTree tree)
+	{
+		for (DEPNode node : tree)
+		{
+			// DO SOMETHING
+			node.getLemma();
+		}
+	}
+}
